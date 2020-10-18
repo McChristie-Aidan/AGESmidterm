2cc0c1ff0253d68dd019f3882a72167b276bf511
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-10-18 18:42:46 -0500

    adds the simpleMovement script and the script folder

---
+++ Assets/Scripts/PlayerSimpleMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSimpleMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 10;
+    [SerializeField]
+    private float maxSpeed = 2;
+    [SerializeField]
+    private PhysicMaterial stoppingMat, movingMat;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+    private new CapsuleCollider collider;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDir = new Vector3(input.x, 0, input.y);
+        inputDir = inputDir.normalized;
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(inputDir * accelerationForce, ForceMode.Acceleration);
+        }
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+}
