78fb9a56a6f725205f13c85d2ad65f9c304dce58
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-11-02 19:51:43 -0600

    adds a death plane to the reset script

--- Assets/Scripts/SceneReset.cs
+++ Assets/Scripts/SceneReset.cs
@@ -5,12 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class SceneReset : MonoBehaviour
 {
-    // Update is called once per frame
+    [SerializeField]
+    private float deathPlaneLevel = -50f;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        GetButtonReset();
+        FellOutOfWorldReset();
+    }
+
+    //resets the level if the player falls below a certain depth.
+    private void FellOutOfWorldReset()
+    {
+        //Debug.Log("object is " + (-1*(deathPlaneLevel - transform.position.y)) + " from the death plane.");
+        if (transform.position.y < deathPlaneLevel)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            ReloadCurrentScene();
         }
+    }
+
+    //resets the level when a certain keyboard key is pressed.
+    private void GetButtonReset()
+    {
+        if (Input.GetButtonDown("Reset"))
+        {
+            ReloadCurrentScene();
+        }
+    }
+
+    //finds what the current scene is and then loads it.
+    private void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
