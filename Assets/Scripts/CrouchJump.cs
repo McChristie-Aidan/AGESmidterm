e1c4fdf6f8b897d82d36edd6db97dee1152fa7c7
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-12-06 22:49:00 -0600

    crouch jump is broken. trying to fix things

--- Assets/Scripts/CrouchJump.cs
+++ Assets/Scripts/CrouchJump.cs
@@ -46,6 +46,7 @@ public class CrouchJump : MonoBehaviour
         //runs if the player is on the ground, is not moving, and is in the crouching state 
         if (characterController.isGrounded == true && player.IsMoving() == false && IsCrouching == true)
         {
+            Debug.Log(IsCrouching);
             chargeTime += chargeSpeed * Time.deltaTime;
 
             //makes sure the superjump wont be higher than the maximum jump height
@@ -54,7 +55,7 @@ public class CrouchJump : MonoBehaviour
                 chargeTime = maxJumpHeight;
             }
 
-            Debug.Log("Current charge = " + chargeTime);
+            //Debug.Log("Current charge = " + chargeTime);
 
             //makes the player jump however high the current charge is
             if (Input.GetButtonDown("Jump"))
