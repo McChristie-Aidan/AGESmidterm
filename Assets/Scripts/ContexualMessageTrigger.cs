8384b8106ccc9fe3bbc76afc8ff425a66da1fc72
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2020-12-12 18:25:25 -0600

    adds the contextual message system and makes it into a prefab

---
+++ Assets/Scripts/ContexualMessageTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ContexualMessageTrigger : MonoBehaviour
+{
+    [SerializeField]
+    [TextArea(3,5)]
+    private string message = "your message here";
+    [SerializeField]
+    private float messageDuration = 1.0f;
+
+    public static Action<string, float> ContextualMessageTriggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (ContextualMessageTriggered != null)
+            {
+                ContextualMessageTriggered.Invoke(message, messageDuration);
+            }
+        }
+    }
+}
